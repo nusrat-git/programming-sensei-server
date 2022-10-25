7ae414bf2f2ef1ecf3a2035231a90de345e87ef3
nusrat-git <nusratjahanmithila66@gmail.com>
2022-10-25 17:10:18 +0600

    added cors

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000
+
+
+app.use(cors())
 
 const categories = require('./data/categories.json');
 const classes = require('./data/classes.json');
 
 
 app.get('/categories', (req, res) => {
-  res.send(categories)
+    res.send(categories)
 })
 
 app.get('/classes', (req, res) => {
-  res.send(classes)
+    res.send(classes)
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
